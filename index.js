cec069b63ec38ea70a9b3e02d3afbca0f81189f2
Yaroslav <hulkzenit35@gmail.com>
2022-11-08 17:41:45 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use('/images', express.static(`${__dirname}/assets/images`));
+app.use('/styles', express.static(`${__dirname}/assets/styles`));
+
+
+app.use('/dashboard', (request, response) => {
+    response.render('dashboard', {
+        title: "Project name",
+        users: 0
+    })
+})
+
+app.listen(3000)
