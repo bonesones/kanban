4247e96460d916a957a10a54e0770fb28c7418df
Carlos <nobody@test.com>
2022-11-12 14:45:20 +0300

    add var.js

---
+++ vars.js
@@ -0,0 +1,25 @@
+const variables = {
+    title: "Project name",
+    tasks: {
+        todo: [
+            {
+                title: "Название задачи",
+                description: "Описание задачи",
+                expires: "1 час"
+            },
+            {
+                title: "Название задачи",
+                description: "Описание задачи",
+                expires: "1 час"
+            },
+            {
+                title: "Название задачи",
+                description: "Описание задачи",
+                expires: "1 час"
+            }
+        ]
+    }
+
+}
+
+module.exports = { variables }
